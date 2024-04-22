6832d10da71649370a02df11ed6796035143f08f
Rumaisa Azeem <rumaisa.a2003@gmail.com>
2024-04-22 02:26:01 +0100

    navbar edits
    - add animations
    - simplify links into loops

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,41 @@
-import {Instagram} from "@mui/icons-material";
+import {Instagram, LinkedIn } from "@mui/icons-material";
 
 export default function Navbar(props) {
+
+    const navLinks = [
+        {
+            'name': 'portfolio',
+            'link': ''
+        },
+        {
+            'name': 'services',
+            'link': ''
+        },
+        {
+            'name': 'contact',
+            'link': ''
+        }
+    ];
+
+    const navIcons = [
+        {
+            'icon': <Instagram/>,
+            'link': 'https://instagram.com/hasanat.shutter'
+        },
+        {
+            'icon': <LinkedIn/>,
+            'link': 'https://linkedin.com/in/hasanat-ali'
+        }
+    ];
+
+
     return (
         <div className="fixed w-screen top-0 z-40 py-4 px-6 flex flex-row gap-4 items-center" style={{color: props.navTextColor}}>
             <div className="font-bold text-2xl">📸 hasanat ali photography</div>
-            <div className="flex gap-4 items-center">
-                <a href="https://instagram.com/hasanat.shutter" target="_blank"><Instagram/></a>
-                <a>portfolio</a>
-                <a>services</a>
-                <a>contact</a>
+            <div className="flex gap-1 items-center">
+                {navIcons.map(item => <a href={item.link} target="_blank" className="hover:translate-y-1 transition ease-in-out">{item.icon}</a>)}
             </div>
+            {navLinks.map(item => <a href={item.link} className="hover:translate-y-1 transition ease-in-out">{item.name}</a>)}
         </div>
     );
 }
