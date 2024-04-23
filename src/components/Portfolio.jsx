72b7900e3dad910925d996829ff6bd3333b582b4
Rumaisa Azeem <rumaisa.a2003@gmail.com>
2024-04-23 15:39:44 +0100

    add routing

---
+++ src/components/Portfolio.jsx
@@ -0,0 +1,10 @@
+import Navbar from "./Navbar";
+
+export default function Portfolio() {
+    return (
+        <div>
+            <Navbar />
+            <h1>Portfolio</h1>
+        </div>
+    )
+}
--- src/index.js
+++ src/index.js
@@ -3,11 +3,24 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Portfolio from './components/Portfolio';
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <App />
+    },
+    {
+        path: "portfolio",
+        element: <Portfolio/>
+    }
+])
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
