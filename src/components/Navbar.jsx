434015e31ead28f35458bd3f5ee81282885104c7
Rumaisa Azeem <rumaisa.a2003@gmail.com>
2024-04-23 15:39:58 +0100

    add camera flash animation to navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,39 +1,53 @@
 import {Instagram, LinkedIn } from "@mui/icons-material";
+import { useState } from 'react';
+
+const navLinks = [
+    {
+        name: 'portfolio',
+        link: '/portfolio'
+    },
+    {
+        name: 'services',
+        link: ''
+    },
+    {
+        name: 'contact',
+        link: ''
+    }
+];
+
+const navIcons = [
+    {
+        icon: <Instagram/>,
+        link: 'https://instagram.com/hasanat.shutter'
+    },
+    {
+        icon: <LinkedIn/>,
+        link: 'https://linkedin.com/in/hasanat-ali'
+    }
+];
 
 export default function Navbar(props) {
 
-    const navLinks = [
-        {
-            'name': 'portfolio',
-            'link': ''
-        },
-        {
-            'name': 'services',
-            'link': ''
-        },
-        {
-            'name': 'contact',
-            'link': ''
-        }
-    ];
+    const flashCameraText = '📸 hasanat ali photography';
+    const noFlashCameraText = '📷 hasanat ali photography';
+    const [brandText, setBrandText] = useState(flashCameraText);
 
-    const navIcons = [
-        {
-            'icon': <Instagram/>,
-            'link': 'https://instagram.com/hasanat.shutter'
-        },
-        {
-            'icon': <LinkedIn/>,
-            'link': 'https://linkedin.com/in/hasanat-ali'
+    const flashCamera = async () => {
+        const wait100 = () => new Promise(resolve => setTimeout(resolve, 100));
+        for (let i = 0; i < 3; i++) {
+            setBrandText(noFlashCameraText);
+            await wait100();
+            setBrandText(flashCameraText);
+            await wait100();
         }
-    ];
-
+    };
 
     return (
         <div className="fixed w-screen top-0 z-40 py-4 px-6 flex flex-row gap-4 items-center" style={{color: props.navTextColor}}>
-            <div className="font-bold text-2xl">📸 hasanat ali photography</div>
+            <a className="font-bold text-2xl" href="/" onMouseEnter={flashCamera}>{brandText}</a>
             <div className="flex gap-1 items-center">
-                {navIcons.map(item => <a href={item.link} target="_blank" className="hover:translate-y-1 transition ease-in-out">{item.icon}</a>)}
+                {navIcons.map(item => <a href={item.link} target="_blank" className="hover:translate-y-1 transition ease-in-out" rel="noreferrer">{item.icon}</a>)}
             </div>
             {navLinks.map(item => <a href={item.link} className="hover:translate-y-1 transition ease-in-out">{item.name}</a>)}
         </div>
