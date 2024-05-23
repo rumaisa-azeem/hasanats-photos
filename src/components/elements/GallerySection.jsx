eba76aa4a19e05f0785cb821040f87c39cfa92a0
Rumaisa Azeem <rumaisa.a2003@gmail.com>
2024-05-23 23:05:18 +0100

    portfolio page

---
+++ src/components/elements/GallerySection.jsx
@@ -0,0 +1,110 @@
+import { Card } from "flowbite-react";
+import {useState} from "react";
+import {Link} from "react-scroll";
+import {ImageList, ImageListItem} from "@mui/material";
+
+export default function GallerySection({sectionTitle, images, id, className}) {
+
+    const itemData = [
+        {
+            image: 'https://images.unsplash.com/photo-1549388604-817d15aa0110',
+            title: 'Bed',
+        },
+        {
+            image: 'https://images.unsplash.com/photo-1525097487452-6278ff080c31',
+            title: 'Books',
+        },
+        {
+            image: 'https://images.unsplash.com/photo-1523413651479-597eb2da0ad6',
+            title: 'Sink',
+        },
+        {
+            image: 'https://images.unsplash.com/photo-1563298723-dcfebaa392e3',
+            title: 'Kitchen',
+        },
+        {
+            image: 'https://images.unsplash.com/photo-1588436706487-9d55d73a39e3',
+            title: 'Blinds',
+        },
+        {
+            image: 'https://images.unsplash.com/photo-1574180045827-681f8a1a9622',
+            title: 'Chairs',
+        },
+        {
+            image: 'https://images.unsplash.com/photo-1530731141654-5993c3016c77',
+            title: 'Laptop',
+        },
+        {
+            image: 'https://images.unsplash.com/photo-1481277542470-605612bd2d61',
+            title: 'Doors',
+        },
+        {
+            image: 'https://images.unsplash.com/photo-1517487881594-2787fef5ebf7',
+            title: 'Coffee',
+        },
+        {
+            image: 'https://images.unsplash.com/photo-1516455207990-7a41ce80f7ee',
+            title: 'Storage',
+        },
+        {
+            image: 'https://images.unsplash.com/photo-1597262975002-c5c3b14bbd62',
+            title: 'Candle',
+        },
+        {
+            image: 'https://images.unsplash.com/photo-1519710164239-da123dc03ef4',
+            title: 'Coffee table',
+        },
+    ];
+
+    return (
+        <div className={"h-screen p-8 " + className} id={id}>
+            <h2 className="font-bold text-4xl mt-14 mb-8">portfolio / <span className="text-neutral-500">{sectionTitle}</span></h2>
+            <div className="h-5/6 overflow-y-scroll bg-neutral-100 p-4">
+                <ImageList cols={4} gap={8} variant="masonry" >
+                    {itemData.map((item, index) =>
+                        <ImageListItem key={index}>
+                            <img
+                                srcSet={`${item.image}`}
+                                src={`${item.image}`}
+                                alt={item.title}
+                                loading="lazy"
+                            />
+                        </ImageListItem>
+                    )}
+                </ImageList>
+            </div>
+        </div>
+    );
+}
+
+
+
+
+/*
+{ sectionTitle &&
+    (
+        <section id="gallery" className="bg-neutral-800 h-min-screen p-8 text-white">
+            <div className="mt-16 flex justify-between items-center relative">
+                <h2 className="text-5xl font-extrabold">portfolio / <span className="text-neutral-400">{sectionTitle}</span></h2>
+                <Link to="gallery-sections" smooth={'easeInOutQuad'} duration={600} className="text-4xl cursor-pointer hover:-translate-y-1 transition ease-in-out">
+                    <KeyboardDoubleArrowUp className="text-white" fontSize="inherit" />
+                </Link>
+            </div>
+            <div className="pt-8">
+                <ImageList cols={3} gap={8} variant="masonry">
+                    {gallerySections.map((item) =>
+                        <ImageListItem key={item}>
+                            <img
+                                srcSet={`${item.image}?w=248&fit=crop&auto=format&dpr=2 2x`}
+                                src={`${item.image}?w=248&fit=crop&auto=format`}
+                                alt={item.title}
+                                loading="lazy"
+                            />
+                        </ImageListItem>
+                    )}
+                </ImageList>
+            </div>
+        </section>
+    )
+}
+ */
--- src/components/pages/Portfolio.jsx
+++ src/components/pages/Portfolio.jsx
@@ -1,11 +1,10 @@
-import {useState, useRef} from "react";
+import {useState} from "react";
 import {Link} from "react-scroll";
 import {Card} from "flowbite-react";
-import {KeyboardDoubleArrowUp} from "@mui/icons-material";
-import {ImageList, ImageListItem} from "@mui/material";
 
 import Navbar from "../sections/Navbar";
 import Footer from "../sections/Footer";
+import GallerySection from "../elements/GallerySection";
 
 import events from "../../assets/gallery/events.jpeg";
 import corporate from "../../assets/gallery/corporate.jpeg";
@@ -37,9 +36,8 @@ const gallerySections = [
 ]
 
 export default function Portfolio() {
-    let [hoveredIndex, setHoveredIndex] = useState(null);
-    let [showHoverEffect, setShowHoverEffect] = useState(false);
-    let [sectionTitle, setSectionTitle] = useState(null);
+    const [hoveredIndex, setHoveredIndex] = useState(null);
+    const [showHoverEffect, setShowHoverEffect] = useState(false);
 
     function handleMouseEnter(index) {
         setHoveredIndex(index);
@@ -51,63 +49,40 @@ export default function Portfolio() {
         setShowHoverEffect(false);
     }
 
-    function handleClick(sectionTitle) {
-        setSectionTitle(sectionTitle);
-    }
-
     return (
         <div>
             <Navbar className="bg-neutral-800 shadow shadow-black" navTextColor="white"/>
-            <section id="gallery-sections">
-                <div className="w-full min-h-screen mt-20 p-6 lg:flex lg:flex-col lg:justify-center xl:mt-0">
-                    <h1 className="text-6xl font-extrabold mb-12">portfolio</h1>
-                    <div className="flex justify-center">
-                        <div className="w-full grid grid-cols-1 gap-4 sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
-                            {gallerySections.map((section, index) =>
-                                <Link to="gallery" smooth={'easeInOutQuad'} duration={600}>
-                                    <Card
-                                        onMouseEnter={() => handleMouseEnter(index)}
-                                        onMouseLeave={handleMouseLeave}
-                                        onClick={() => handleClick(section.title)}
-                                        key={index}
-                                        imgSrc={section.image}
-                                        className={"w-full hover:translate-y-2 transition duration-300 cursor-pointer ease-in-out" + (showHoverEffect && index!==hoveredIndex ? "filter grayscale" : "")}
-                                    >
-                                        <h2 className="text-xl lg:text-2xl font-bold">{section.title}</h2>
-                                    </Card>
-                                </Link>
-                            )}
+            <div id="scroll-sections" className="h-screen overflow-y-scroll snap-y">
+                <div id="gallery-sections" className="snap-start">
+                    <div className="w-full min-h-screen mt-20 p-6 lg:flex lg:flex-col lg:justify-center xl:mt-0">
+                        <div className="mb-12">
+                            <h1 className="text-6xl font-extrabold mb-4">portfolio</h1>
+                            <p className="text-lg">here's some of my work...</p>
+                        </div>
+                        <div className="flex justify-center">
+                            <div className="w-full grid grid-cols-2 gap-4 sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
+                                {gallerySections.map((section, index) =>
+                                    <Link to={"gallery-section-" + index} containerId="scroll-sections" smooth={'easeInOutQuad'} duration={600}>
+                                        <Card
+                                            onMouseEnter={() => handleMouseEnter(index)}
+                                            onMouseLeave={handleMouseLeave}
+                                            key={index}
+                                            imgSrc={section.image}
+                                            className={"w-full hover:translate-y-2 transition duration-300 cursor-pointer ease-in-out" + (showHoverEffect && index!==hoveredIndex ? "filter grayscale" : "")}
+                                        >
+                                            <h2 className="text-xl lg:text-2xl font-bold">{section.title}</h2>
+                                        </Card>
+                                    </Link>
+                                )}
+                            </div>
                         </div>
                     </div>
                 </div>
-            </section>
-            { sectionTitle &&
-                (
-                    <section id="gallery" className="bg-neutral-800 h-min-screen p-8 text-white">
-                        <div className="mt-16 flex justify-between items-center relative">
-                            <h2 className="text-5xl font-extrabold">portfolio / <span className="text-neutral-400">{sectionTitle}</span></h2>
-                            <Link to="gallery-sections" smooth={'easeInOutQuad'} duration={600} className="text-4xl cursor-pointer hover:-translate-y-1 transition ease-in-out">
-                                <KeyboardDoubleArrowUp className="text-white" fontSize="inherit" />
-                            </Link>
-                        </div>
-                        <div className="pt-8">
-                            <ImageList cols={3} gap={8} variant="masonry">
-                                {gallerySections.map((item) =>
-                                    <ImageListItem key={item}>
-                                        <img
-                                            srcSet={`${item.image}?w=248&fit=crop&auto=format&dpr=2 2x`}
-                                            src={`${item.image}?w=248&fit=crop&auto=format`}
-                                            alt={item.title}
-                                            loading="lazy"
-                                        />
-                                    </ImageListItem>
-                                )}
-                            </ImageList>
-                        </div>
-                    </section>
-                )
-            }
-            <Footer className="bg-neutral-800"/>
+                {gallerySections.map((section, index) =>
+                    <GallerySection sectionTitle={section.title} key={index} id={"gallery-section-"+index} className="snap-start" images={gallerySections} />
+                )}
+                <Footer className="bg-neutral-800 snap-center"/>
+            </div>
         </div>
     )
 }
