91e093229a0198fd9a86e77f21b84cb2cca94b2b
Rumaisa Azeem <rumaisa.a2003@gmail.com>
2024-10-05 20:35:26 +0100

    update footer link

--- src/components/sections/Footer.jsx
+++ src/components/sections/Footer.jsx
@@ -1,7 +1,7 @@
 export default function Footer(props) {
     return (
         <footer className={"border-t-2 border-t-neutral-700 text-neutral-400 p-10 " + props.className}>
-            <p className="text-center">Copyright © 2024 Abul Hasanat Ali, all rights reserved. Designed and coded by <a href={"https://rumaisacodes.com"} className={"underline"}>Rumaisa Azeem.</a></p>
+            <p className="text-center">Copyright © 2024 Abul Hasanat Ali, all rights reserved. Designed and coded by <a href={"https://rumaisacodes.com"} target={"_blank"} className={"underline"}>Rumaisa Azeem.</a></p>
         </footer>
     );
 }
